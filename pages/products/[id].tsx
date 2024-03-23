51a6a7fa618608d7d3c454e6ebd5a845479d5f2d
luuyen01 <luuyen080@gmail.com>
2024-03-23 16:09:57 +0700

    product detail

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,38 +1,30 @@
 import type {NextPage} from 'next'
 import {useRouter} from "next/router";
 import {DefaultLayout} from "../../components/layout";
-import Description from "../../components/description";
 import React, {useEffect, useState} from "react";
-import {IProduct} from "./all-product";
 import {Product} from "../../model/Product";
 import {ProductDetail} from "../../model/ProductDetail";
-import ImageProductDetail from "../../components/image-product-detail.";
-import {Button, Card, Col, Flex, InputNumber, Row} from "antd";
-import AddToCard from "../../components/add-to-card";
+import ProductDetailComponent from "../../components/product-detail.component";
+
+export interface IProduct {
+    id: string;
+    name: string | null;
+    shortDescription: string | null;
+    productDetails: IProductDetail[];
+}
 
 export interface IProductDetail {
     id: string;
-    name: string;
-    price: number | null;
+    name: string | null;
+    price: number,
     description: string | null;
-    images: string[] | null;
-    status: boolean;
+    images: string[] | null
 }
 
 const ProductDetails: NextPage = () => {
     const router = useRouter();
     const {id} = router.query;
     const [product, setProduct] = useState<IProduct>();
-    const defaultProduct: IProduct = {
-        id: "",
-        name: "",
-        thumbnail: null,
-        shortDescription: null,
-        status: false,
-        minPrice: 0,
-        maxPrice: 0,
-        productDetails: []
-    }
 
     useEffect(() => {
         getProductDetailById();
@@ -45,34 +37,30 @@ const ProductDetails: NextPage = () => {
             .then(response => {
                 const product: Product = response;
 
-                const mappedProductDetail: IProduct = {
+                // @ts-ignore
+                const mappedProductl: IProduct = {
                     id: product.id,
                     name: product.name,
-                    thumbnail: product.thumbnail,
                     shortDescription: product.shortDescription,
-                    status: product.status,
-                    minPrice: Math.min(...product.productDetails.map((productDetail) => productDetail.price)),
-                    maxPrice: Math.max(...product.productDetails.map((productDetail) => productDetail.price)),
                     productDetails: (product.productDetails as ProductDetail[]).map((productDetail: ProductDetail): IProductDetail => {
                         return {
                             id: productDetail.id,
                             name: productDetail.name,
                             price: productDetail.price,
                             description: productDetail.description,
-                            images: productDetail.images,
-                            status: productDetail.status
+                            images: productDetail.images
                         }
                     })
                 };
 
-                setProduct(mappedProductDetail);
+                setProduct(mappedProductl);
             })
             .catch(response => console.log(response))
     }
 
     return (
         <DefaultLayout>
-            <Description product={product ? product : defaultProduct}></Description>
+            <ProductDetailComponent product={product ? product : null} firstProductDetail={product?.productDetails ? product.productDetails?.[0] : null}></ProductDetailComponent>
         </DefaultLayout>
     );
 }
