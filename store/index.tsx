f90c210d287875c949eaa19b43634dc739279e9a
admin <admin@admin-2.local>
2024-03-24 20:21:37 +0700

    update 24032024

--- store/index.tsx
+++ store/index.tsx
@@ -9,7 +9,7 @@ const store = configureStore({
     reducer: rootReducer,
 });
 
-export default store;
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export default store;
