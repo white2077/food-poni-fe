507db721dd4217ee356079b3f83a3d030a7b6e00
luuyen01 <luuyen080@gmail.com>
2024-03-26 21:10:18 +0700

    checkout

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,14 +1,24 @@
 import {DefaultLayout} from "../components/layout";
-import {Card, Checkbox, Col, Row, Space} from "antd";
+import {Button, Card, Checkbox, Col, Divider, Row, Space} from "antd";
 import {DeleteOutlined} from "@ant-design/icons";
+import {useDispatch, useSelector} from "react-redux";
+import {ICartItem} from "../store/cart.reducer";
+import {CurrentUser} from "../model/User";
 
 const Checkout = () => {
+
+    const dispatch = useDispatch();
+
+    const cartItems = useSelector(state => state.cart.cartItems) as ICartItem[];
+
+    const currentUser = useSelector(state => state.user.currentUser) as CurrentUser;
+
     return (
         <DefaultLayout>
             <div style={{color: "black", textAlign: "left"}}>
                 <h1>GIỎ HÀNG</h1>
                 <Row gutter={16}>
-                    <Col flex={3}>
+                    <Col flex='auto'>
                         <Card style={{marginBottom: "16px"}}>
                             <Row>
                                 <Col flex='5%'><Checkbox></Checkbox></Col>
@@ -16,7 +26,7 @@ const Checkout = () => {
                                 <Col flex='15%'>Đơn giá</Col>
                                 <Col flex='10%'>Số lượng</Col>
                                 <Col flex='15%'>Thành tiền</Col>
-                                <Col flex='5%'><DeleteOutlined /></Col>
+                                <Col flex='5%'><DeleteOutlined/></Col>
                             </Row>
                         </Card>
                         <Card>
@@ -26,17 +36,37 @@ const Checkout = () => {
                                 <Col flex='15%'>Đơn giá</Col>
                                 <Col flex='10%'>Số lượng</Col>
                                 <Col flex='15%'>Thành tiền</Col>
-                                <Col flex='5%'><DeleteOutlined /></Col>
+                                <Col flex='5%'><DeleteOutlined/></Col>
                             </Row>
                         </Card>
                     </Col>
-                    <Col flex={1}>
+                    <Col flex='450px'>
                         <Card style={{marginBottom: "16px"}}>
-                            1 / 4
+                            <div>
+                                Giao tới
+                                <a style={{float: 'right'}} href="">Thay đổi</a>
+                            </div>
+                            <div>
+                                <div>Luu Thi Hai Yen | 0326207815</div>
+                                <div>Tòa ViwaSeen 48 Tố Hữu, Phường Trung Văn, Quận Nam Từ Liêm, Hà Nội</div>
+                            </div>
                         </Card>
-                        <Card>
-                            1 / 4
+                        <Card style={{marginBottom: "16px"}}>
+                            <div>
+                                Tạm tính
+                                <span style={{float: 'right'}}>0</span>
+                            </div>
+                            <div>
+                                Giảm giá
+                                <span style={{float: 'right'}}>0</span>
+                            </div>
+                            <Divider/>
+                            <div>
+                                Tổng tiền
+                                <span style={{float: 'right'}}>0</span>
+                            </div>
                         </Card>
+                        <Button type="primary" danger block>Thanh toán</Button>
                     </Col>
                 </Row>
             </div>
