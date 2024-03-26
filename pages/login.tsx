5995a369da63ccbf0641e6de6f3546b150b5b5e9
luuyen01 <luuyen080@gmail.com>
2024-03-26 13:14:16 +0700

    login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,19 +1,144 @@
 import type {NextPage} from 'next'
+import {Avatar, Button, Card, Checkbox, Form, Input, notification, Space} from 'antd';
+import {DefaultLayout} from "../components/layout";
+import {GithubOutlined, GoogleOutlined, LockOutlined, UserOutlined} from "@ant-design/icons";
+import {useState} from "react";
+import axiosConfig from "../utils/axios-config";
+import {CurrentUser, IUserRemember, UserRequestLogin} from "../model/User";
+import {IToken} from "../model/Auth";
+import {jwtDecode} from "jwt-decode";
+import {deleteCookie, setCookie} from "cookies-next";
+import {REFRESH_TOKEN, REMEMBER_ME} from "../utils/server";
+import {useRouter} from "next/router";
 
-export interface IPost {
-    id: number;
-    title: string;
-    content: string;
-    image: string;
-    date: string;
-}
+const isPending: boolean = false;
+const isCurrentUser: CurrentUser = {
+    id: '',
+    sub: '',
+    roles: [],
+    fullName: '',
+    avatar: '',
+    email: '',
+    phoneNumber: '',
+    username: '',
+    accessToken: ''
+};
 
 const Login: NextPage = () => {
 
+    const [currentUser, setCurrentUser] = useState<CurrentUser>(isCurrentUser);
+
+    const [pending, setPending] = useState<boolean>(isPending);
+
+    const router = useRouter();
+
+    const onFinish = (values: any) => {
+        setPending(true);
+        let user: UserRequestLogin = values.username.includes("@")
+            ? {username: null, email: values.username, password: values.password}
+            : {username: values.username, email: null, password: values.password}
+
+        axiosConfig.post("/auth/login", user)
+            .then(function (res: any) {
+                setPending(false);
+
+                const {accessToken, refreshToken} = res.data as IToken;
+                setCurrentUser(jwtDecode(accessToken) as CurrentUser);
+
+                // set refresh token
+                setCookie(REFRESH_TOKEN, refreshToken, {
+                    expires: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 30),
+                });
+
+                //set user remembered and delete
+                if (values.remember) {
+                    const userRemember: IUserRemember = {
+                        username: user.username ? user.username! : user.email!,
+                        password: user.password,
+                        avatar: currentUser.avatar
+                    }
+                    setCookie(REMEMBER_ME, btoa(JSON.stringify(userRemember)), {
+                        expires: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 30),
+                    });
+                } else deleteCookie(REMEMBER_ME);
+
+                // redirect to home page
+                notification.open({
+                    message: 'Login message',
+                    description: 'Login successfully!',
+                });
+                router.push('/');
+            })
+            .catch(function (res) {
+                setPending(false);
+                notification.open({
+                    message: 'Login message',
+                    description: res.message,
+                });
+            })
+    };
+
     return (
-        <div>
-            Login page
-        </div>
+        <DefaultLayout>
+            <Card style={{width: "500px", margin: "auto"}}>
+                <Space direction="vertical" size="middle" style={{display: 'flex'}}>
+                    <div>
+                        <Avatar size={64} icon={<UserOutlined/>}/>
+                        <h2>Welcome back!...</h2>
+                        <span>Sign in to your account to continue</span>
+                    </div>
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                        initialValues={{remember: true}}
+                        onFinish={onFinish}
+                    >
+                        <Form.Item
+                            name="username"
+                            rules={[{required: true, message: 'Please input your Username!'}]}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Username"/>
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[{required: true, message: 'Please input your Password!'}]}
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon"/>}
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+
+                            <a className="login-form-forgot" href="">
+                                Forgot password
+                            </a>
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button" loading={pending}
+                                    block>
+                                Log in
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                    <div>
+                        <div style={{float: 'left'}}>
+                            <Space>
+                                Other login method
+                                <GithubOutlined/>
+                                <GoogleOutlined/>
+                            </Space>
+                        </div>
+                        <a style={{float: 'right'}} href="">Register now</a>
+                    </div>
+                </Space>
+            </Card>
+        </DefaultLayout>
     );
 }
 
--- utils/server.ts
+++ utils/server.ts
@@ -1,3 +1,11 @@
 const dev = process.env.NODE_ENV !== 'production';
 
+export const REMEMBER_ME = 'rememberMe';
+
+export const ACCESS_TOKEN = 'accessToken';
+
+export const REFRESH_TOKEN = 'refreshToken';
+
+export const HEADER_TOKEN = 'Authorization';
+
 export const server = dev ? 'http://localhost:8080/api/v1' : 'https://production.server.com';
