f32991ceece8c334902fbfcd41aa8dce9a830aec
admin <luuyen080@gmail.com>
2024-07-21 13:41:00 +0700

    luuyen-update personal information-21072024

--- components/comboBox_date.tsx
+++ components/comboBox_date.tsx
@@ -1,61 +1,61 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 import {Select} from "antd";
 
 
 const ComboBoxDate = () => {
-  const [selectedDay, setSelectedDay] = useState<number | ''>('');
-  const [selectedMonth, setSelectedMonth] = useState<number | ''>('');
-  const [selectedYear, setSelectedYear] = useState<number | ''>('');
+    const [selectedDay, setSelectedDay] = useState<number | ''>(29);
+    const [selectedMonth, setSelectedMonth] = useState<number | ''>(10);
+    const [selectedYear, setSelectedYear] = useState<number | ''>(2001);
 
-  const days = Array.from({ length: 31 }, (_, index) => index + 1);
-  const months = Array.from({ length: 12 }, (_, index) => index + 1);
-  const [years, setYears] = useState<number[]>([]);
+    const days = Array.from({length: 31}, (_, index) => index + 1);
+    const months = Array.from({length: 12}, (_, index) => index + 1);
+    const [years, setYears] = useState<number[]>([]);
 
-  useEffect(() => {
-    const currentYear = new Date().getFullYear();
-    const yearsList = Array.from({ length: currentYear - 1900 + 1 }, (_, index) => currentYear - index);
-    setYears(yearsList);
-  }, []);
+    useEffect(() => {
+        const currentYear = new Date().getFullYear();
+        const yearsList = Array.from({length: currentYear - 1900 + 1}, (_, index) => currentYear - index);
+        setYears(yearsList);
+    }, []);
 
-  const handleDayChange = (value: number | '') => {
-    setSelectedDay(value);
-  };
+    const handleDayChange = (value: number | '') => {
+        setSelectedDay(value);
+    };
 
-  const handleMonthChange = (value: number | '') => {
-    setSelectedMonth(value);
-  };
+    const handleMonthChange = (value: number | '') => {
+        setSelectedMonth(value);
+    };
 
-  const handleYearChange = (value: number | '') => {
-    setSelectedYear(value);
-  };
+    const handleYearChange = (value: number | '') => {
+        setSelectedYear(value);
+    };
 
-  return (
-      <div className="col-span-3 w-full">
-        <div className="flex justify-start items-top w-full gap-5">
-          <Select className="w-28" value={selectedDay} onChange={handleDayChange}>
-            {days.map((day) => (
-                <Select.Option key={day} value={day}>
-                  {day}
-                </Select.Option>
-            ))}
-          </Select>
-          <Select className="w-28" value={selectedMonth} onChange={handleMonthChange}>
-            {months.map((month) => (
-                <Select.Option key={month} value={month}>
-                  {month}
-                </Select.Option>
-            ))}
-          </Select>
-          <Select className="w-28" value={selectedYear} onChange={handleYearChange}>
-            {years.map((year) => (
-                <Select.Option key={year} value={year}>
-                    {year}
-                </Select.Option>
-            ))}
-          </Select>
+    return (
+        <div className="col-span-3 w-full">
+            <div className="flex justify-start items-top w-full gap-5">
+                <Select className="w-28" value={selectedDay} onChange={handleDayChange}>
+                    {days.map((day) => (
+                        <Select.Option key={day} value={day}>
+                            {day}
+                        </Select.Option>
+                    ))}
+                </Select>
+                <Select className="w-28" value={selectedMonth} onChange={handleMonthChange}>
+                    {months.map((month) => (
+                        <Select.Option key={month} value={month}>
+                            {month}
+                        </Select.Option>
+                    ))}
+                </Select>
+                <Select className="w-28" value={selectedYear} onChange={handleYearChange}>
+                    {years.map((year) => (
+                        <Select.Option key={year} value={year}>
+                            {year}
+                        </Select.Option>
+                    ))}
+                </Select>
+            </div>
         </div>
-      </div>
-  );
+    );
 };
 
 export default ComboBoxDate;
--- components/personal-information.tsx
+++ components/personal-information.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import {Button, DatePicker, Image, Input, Radio, Select, Space} from "antd";
+import {Button, Image, Input, Radio, Select, Space} from "antd";
 import {DeleteOutlined, LockOutlined, MailOutlined, PhoneOutlined, SafetyCertificateOutlined} from "@ant-design/icons";
 import {CurrentUser} from "../stores/user.reducer";
 import {useSelector} from "react-redux";
 import {RootState} from "../stores";
 import {server} from "../utils/server";
-import Line from "./line_custom";
 import ComboBoxDate from "./comboBox_date";
-import Marquee from "react-fast-marquee";
 
 export const PersonalInformation = () => {
 
@@ -60,63 +58,32 @@ export const PersonalInformation = () => {
                         </div>
                     </div>
                     <div>
-                        <Radio.Group>
+                        <Radio.Group defaultValue={true}>
                             <Space direction="vertical">
                                 <div className="flex gap-2">
-                                    <Radio value="1">
+                                    <Radio value={true}>
                                         <p>Nam</p>
                                     </Radio>
-                                    <Radio value="2">
+                                    <Radio value={false}>
                                         <p>Nữ</p>
                                     </Radio>
-                                    <Radio value="3">
-                                        <p>Khác</p>
-                                    </Radio>
+                                    {/*<Radio value="3">*/}
+                                    {/*    <p>Khác</p>*/}
+                                    {/*</Radio>*/}
                                 </div>
                             </Space>
                         </Radio.Group>
                     </div>
                 </div>
-                <div className="grid lg:grid-cols-4 mt-8">
-                    <div className="col-span-1 w-[100%]">
-                        <div className="flex justify-start items-center gap-5">
-                            Quốc tịch
-                        </div>
-                    </div>
-                    <div className="col-span-3">
-                        <Select className="w-full">
-                            <Select.Option value="1">Việt Nam</Select.Option>
-                            <Select.Option value="2">Hàn Quốc</Select.Option>
-                            <Select.Option value="3">Nhật Bản</Select.Option>
-                            <Select.Option value="4">Mỹ</Select.Option>
-                        </Select>
-                    </div>
-                </div>
-                <div className="grid lg:grid-cols-4 mt-8">
-                    <div className="col-span-1 w-[100%]">
-                    </div>
-                    <div className="col-span-3">
-                        <Button className="w-44">Lưu thay đổi</Button>
-                    </div>
+                <div className="mt-8 text-center">
+                    <Button className="w-44">Lưu thay đổi</Button>
                 </div>
             </div>
             <div className="col-span-2">
                 <div className="grid lg:grid-cols-2">
                     <div className="border-l-2 col-span-2">
                         <div className="px-4">
-                            <h3 className="text-[17px] font-sans text-gray-400">Số điện thoại và Email</h3>
-                            <div className="p-4 grid gap-3">
-                                <div className="flex items-center gap-2 justify-between w-[100%]">
-                                    <div className="flex items-center gap-2 w-[100%]">
-                                        <PhoneOutlined/>
-                                        <div className="text-lg text-gray-600 ml-2 ">
-                                            <div className="text-[15px]">Số điện thoại</div>
-                                            <div className="min-w-[150px] text-[15px]">0123456789</div>
-                                        </div>
-                                    </div>
-                                    <Button>Cập nhật</Button>
-                                </div>
-                            </div>
+                            <h3 className="text-[17px] font-sans text-gray-400">Email</h3>
                             <div className="p-4 grid gap-3">
                                 <div className="flex items-center gap-2 justify-between w-[100%]">
                                     <div className="flex items-center gap-2 w-[100%]">
