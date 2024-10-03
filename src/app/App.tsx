a342581ff04ecab0c57639c3e512a41d12562076
Hoang Dung <dungbui8198@gmail.com>
2024-10-03 09:39:06 +0700

    add user to redux
    check

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,40 @@
-import {Suspense} from 'react'
+import {Suspense, useEffect} from 'react'
 import {Outlet} from 'react-router-dom'
 import {LayoutProvider, LayoutSplashScreen} from '@/_metronic/layout/core'
 import {AuthInit} from './modules/auth'
+import Cookies from "js-cookie";
+import {REFRESH_TOKEN} from "@/utils/server.ts";
+import jwtDecode from "jwt-decode";
+import {useDispatch} from "react-redux";
+import {updateCurrentUser} from "@/redux/modules/auth.ts";
 
 const App = () => {
-  return (
-    <Suspense fallback={<LayoutSplashScreen />}>
-        <LayoutProvider>
-            <AuthInit>
-              <Outlet />
-            </AuthInit>
-        </LayoutProvider>
-    </Suspense>
-  )
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const refresh_token = Cookies.get(REFRESH_TOKEN);
+        if (refresh_token) {
+            const payload: {
+                readonly role: string,
+                readonly id: string,
+                readonly avatar: string,
+                readonly email: string,
+                readonly addressId: string,
+                readonly username: string
+            } = jwtDecode(refresh_token);
+            dispatch(updateCurrentUser(payload));
+        }
+    }, [dispatch]);
+
+    return (
+        <Suspense fallback={<LayoutSplashScreen/>}>
+            <LayoutProvider>
+                <AuthInit>
+                    <Outlet/>
+                </AuthInit>
+            </LayoutProvider>
+        </Suspense>
+    )
 }
 
 export {App}
