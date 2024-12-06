4b65de49381fb47a15e9abb1d95765bfbfd9b886
Hoang Dung <dungbui8198@gmail.com>
2024-12-06 12:25:29 +0700

    deploy

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -8,6 +8,6 @@ export const HEADER_TOKEN: string = "Authorization";
 
 export const server: string =
   process.env.NODE_ENV === "production"
-    ? "http://192.168.1.8:8080"
+    ? "https://curly-space-dollop-p9w4vv6547ph6pgr-8080.app.github.dev"
     : // : 'http://34.92.177.122:8080';
-      "http://localhost:8080";
+      "https://curly-space-dollop-p9w4vv6547ph6pgr-8080.app.github.dev";
